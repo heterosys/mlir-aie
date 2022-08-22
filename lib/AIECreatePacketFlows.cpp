e85ea3ef47d10deed4b78ea8b8a0d426a20da6b4
Jason Lau <lau@cs.ucla.edu>
2022-08-22 11:41:57 -0600

    Remove duplicate AIEOpRemoval

--- lib/AIECreatePacketFlows.cpp
+++ lib/AIECreatePacketFlows.cpp
@@ -25,25 +25,6 @@ using namespace mlir;
 using namespace xilinx;
 using namespace xilinx::AIE;
 
-template <typename MyOp>
-struct AIEOpRemoval : public OpConversionPattern<MyOp> {
-  using OpConversionPattern<MyOp>::OpConversionPattern;
-  using OpAdaptor = typename MyOp::Adaptor;
-  ModuleOp &module;
-
-  AIEOpRemoval(MLIRContext *context, ModuleOp &m, PatternBenefit benefit = 1)
-      : OpConversionPattern<MyOp>(context, benefit), module(m) {}
-
-  LogicalResult
-  matchAndRewrite(MyOp op, OpAdaptor adaptor,
-                  ConversionPatternRewriter &rewriter) const override {
-    Operation *Op = op.getOperation();
-
-    rewriter.eraseOp(Op);
-    return success();
-  }
-};
-
 // A port on a switch is identified by the tile and port name.
 typedef std::pair<Operation *, Port> PhysPort;
 
